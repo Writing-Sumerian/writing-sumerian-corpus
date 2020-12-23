8c86cea42b590c643d8e2dade39a925e7aaf1961
Marc Endesfelder <marc.endesfelder@gmail.com>
2020-12-22 21:27:15 -0500

    Rework search

--- src/search/python/cuneiform_search.py
+++ src/search/python/cuneiform_search.py
@@ -1,147 +1,234 @@
 from typing import List
+from py2plpy import plpy
+
 
 def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str:
-    """COST 100 IMMUTABLE"""
+    """COST 100 STABLE"""
      
-    import re
+    from lark import Lark
+    grammar = r"""
+    start: (line|word) (_space (line|word))*
+    line: "[" word (_nbspace word)* "]"
+    word: _part (_sep _part)*
+    _sep: dots?_SEP(dots _SEP?)?
+    _space: (_SPACE|dots|wordbreak|linebreak)+
+    _nbspace: (_SPACE|dots|wordbreak)+
+    _part: lindicator* char rindicator* | indicator+
+    lindicator: indicator
+    rindicator: indicator 
+    indicator: det | pc
+    det: "{" char (_sep char)* "}"
+    pc: "<" char (_sep char)* ">"
+    char: VALUE _signspec?   -> value
+        | SIGN              -> sign
+        | PATTERN _signspec? -> pattern
+        | _SIGNX            -> signx
+        | _VALUEX _signspec? -> valuex
+        | _N                -> n
+    _signspec: "(" SIGN ")"
 
-    LCHARS = 'abdegĝhḫijklmnpqrsšṣtṭuwyz’'
-    UCHARS = 'ABDEGĜHḪIJKLMNPQRSŠṢTṬUWYZ’'
-    OPERATORS = r'\- ;‹›…<>{}'
-    VALUE = re.compile(r'[{lchars}]+(?:[1-9][0-9]*)?'.format(lchars = LCHARS))
-    SIGN = re.compile(r'(?:[{uchars}]+(?:[1-9][0-9]*)?|[0-9]+|(?:LAK|KWU|REC)[0-9]+[a-c]?)(?:@(?:[tgšc]|90|180))*'.format(uchars = UCHARS))
+    dots: _DOTS
+    wordbreak: ";"
+    linebreak: _NL
 
+    SIGN.3: /[ABDEGĜHḪIJKLMNPQRSŠṢTṬUWYZ’]+[0-9]*/
+    VALUE.2: /[abdegĝhḫijklmnpqrsšṣtṭuwyz’]+[0-9xX]*/
+    _SIGNX.1: "X"
+    _VALUEX.1: "x"
+    _N.1: /[Nn]/
+    PATTERN.0: /\/[^\/]*\//
+    _DOTS.1: "…"|"..."
+    _SEP.0: /[.-]/
+    _SPACE: /[\t \f\n]+/
+    _NL: "//"
+    """
+    l = Lark(grammar, lexer='standard')
 
-    def preprocess(term):
-        
-        term = term.replace('.', '-')
-        term = term.replace('_', '.')
-
-        term = re.sub(r'[^ ]+‹[^ ]*', r'»\0', term)
-
-        term = re.sub(r'{([^}]*)}(?=[ \-;]|$)', r'{{\1}', term)
-        term = re.sub(r'<([^>]*)>(?=[ \-;]|$)', r'<<\1>', term)
-
-        return term
-
-
-    class State:
+    class Listener:
         def __init__(self):
-            self.gap = False
-            self.part = ''
-            self.con = ' '
-            self.type = ''
+            self.consecutive = None
+            self.sameWord = None
+            self.sameWordNext = None
+            self.sameLine = None
+            self.sameLineNext = None
 
-        def processOp(self, op):
+            self.alignment = None
+            self.indicator = None
+            self.phonographic = None
 
-            if op == ' ':
-                self.con = op
-                self.part = ''
-                self.type = ''
-            elif op == ';':
-                self.con = op
-                self.part = ''
-                self.type = ''
-            elif op == '-':
-                self.con = op
+            self.previous_sign = False
 
-            elif op == '»':
-                self.part = 'PREFIX'
-            elif op == '‹':
-                self.part = 'STEM'
-            elif op == '›':
-                self.part = 'SUFFIX'
+            self.i = 0
+            self.conditions = []
+            self.sources = []
 
-            elif op == '{':
-                self.type = 'DETS' if self.type == 'DETP' else 'DETP'
-            elif op == '}':
-                self.type = ''
-            elif op == '<':
-                self.type = 'PCS' if self.type == 'PCP' else 'PCP'
-            elif op == '>':
-                self.type = ''
+        def commit(self, sign):
+            if self.i:
+                self.conditions += [f'c{self.i}.{key} = c0.{key}' for key in target_key]
 
-            elif op == '…':
-                self.gap = True
+                if self.consecutive:
+                    if self.previous_sign and sign:
+                        self.conditions.append(f'c{self.i}.component_no = c{self.i-1}.component_no+1')
+                    else:
+                        self.conditions.append(f'c{self.i}.sign_no = c{self.i-1}.sign_no+1')
+                        if self.previous_sign:
+                            self.conditions.append(f'c{self.i-1}.final')
+                        elif sign:
+                            self.conditions.append(f'c{self.i}.initial')
+                else:
+                    if self.previous_sign and sign:
+                        self.conditions.append(f'c{self.i}.component_no > c{self.i-1}.component_no')
+                    else:
+                        self.conditions.append(f'c{self.i}.sign_no > c{self.i-1}.sign_no')
+                if self.sameWord:
+                    self.conditions.append(f'c{self.i}.word_no = c{self.i-1}.word_no')
+                elif self.sameWord == False:
+                    self.conditions.append(f'c{self.i}.word_no > c{self.i-1}.word_no')
+                if self.sameLine:
+                    self.conditions.append(f'c{self.i}.line_no = c{self.i-1}.line_no')
+                elif self.sameLine == False:
+                    self.conditions.append(f'c{self.i}.line_no > c{self.i-1}.line_no')
 
-        def getType(self):
-            return self.type if self.type else self.part
+            if self.alignment is not None:
+                self.conditions.append(f"(c{self.i}.properties).alignment = '{self.alignment}'")
+            if self.indicator is not None:
+                self.conditions.append(f"(c{self.i}.properties).indicator = '{self.indicator}'")
+            if self.phonographic is not None:
+                self.conditions.append(f"(c{self.i}.properties).phonographic = '{self.phonographic}'")
 
-
-    def join_clause(a, b):
-        return '{b} ON ('.format(b=b)+' AND '.join(['{a}.{col} = {b}.{col}'.format(a=a, b=b, col=col) for col in target_key])+')'
-
-
-    state = State()
-    conditions = []
-    ctes = []
-    cte_joins = []
-
-    term = preprocess(search_term)
-    tokens = re.findall(r'[{op}]|[^{op}]+'.format(op=OPERATORS), term)
-
-    if tokens[0] in ';‹':
-        from_clause = '{table} c JOIN {table} {join_clause}'.format(table=target_table, join_clause=join_clause('c', 'c0'))
-        if tokens[0] == ';':
-            conditions.append('((c0.sign_no = c.sign_no + 1 AND c.word_no != c0.word_no) OR (c0.sign_no = c.sign_no AND c.sign_no = 0))')
-        elif tokens[0] == '‹':
-            conditions.append("((c0.sign_no = c.sign_no + 1 AND c.stem = FALSE) OR (c0.sign_no = c.sign_no AND c.sign_no = 0))")
-    else:
-        from_clause = '{table} c0'.format(table=target_table)
-
-    i = 0
-    for token in tokens:
-
-        if token in OPERATORS:
-            state.processOp(token)
-            continue
-
-        # Connectors
-        if i:
-            if state.gap:
-                conditions.append('c{i}.sign_no > c{i_p}.sign_no'.format(i = str(i), i_p = str(i-1)))
-                state.gap = False 
+            if sign:
+                self.sources.append(f'corpus_composition c{self.i}')
             else:
-                conditions.append('c{i}.sign_no = c{i_p}.sign_no + 1'.format(i = str(i), i_p = str(i-1)))
+                self.sources.append(f'corpus c{self.i}')
 
-            if state.con == '-':
-                conditions.append('c{i}.word_no = c{i_p}.word_no'.format(i = str(i), i_p = str(i-1)))
-            elif state.con == ';':
-                conditions.append('c{i}.word_no != c{i_p}.word_no'.format(i = str(i), i_p = str(i-1)))
+            self.i += 1
+            self.consecutive = True
+            self.initialSameWord = False
+            self.initialSameLine = False
+            if self.sameWord == False:
+                self.sameWord = None
+            if self.sameWordNext is not None:
+                self.sameWord = self.sameWordNext
+                self.sameWordNext = None
+            if self.sameLine == False:
+                self.sameLine = None
+            if self.sameLineNext is not None:
+                self.sameLine = self.sameLineNext
+                self.sameLineNext = None
+            self.previous_sign = sign
 
-        # Values
-        if token == 'X':
-            pass
-        elif VALUE.fullmatch(token):
-            ctes.append("cte{i} AS (SELECT value_id FROM value_variants WHERE value = '{sign}')".format(i = str(i), sign = token))
-            cte_joins.append("cte{i} ON c{i}.value_id = cte{i}.value_id".format(i = str(i)))
-        elif SIGN.fullmatch(token):
-            ctes.append("cte{i} AS (SELECT sign_id FROM value_variants JOIN values USING (value_id) WHERE value = '{sign}')".format(i = str(i), sign = token.lower()))
-            cte_joins.append("cte{i} ON c{i}.sign_id = cte{i}.sign_id".format(i = str(i)))
-        else:
-            ctes.append("cte{i} AS (SELECT value_id FROM value_variants WHERE value ~ '^{sign}$')".format(i = str(i), sign = token))
-            cte_joins.append("cte{i} ON c{i}.value_id = cte{i}.value_id".format(i = str(i)))
+        def compose(self):
+            fromClause = ', '.join(self.sources)
+            whereClause = ' AND '.join(self.conditions)
+            arrayClause = ', '.join(f'c{i}.sign_no' for i in range(self.i))
+            keys = ', '.join([f'c0.{key}' for key in target_key])
+            return f'SELECT {keys}, ARRAY[{arrayClause}] AS signs FROM {fromClause} WHERE {whereClause};'
+        
+        def visit(self, node):
 
-        # Types
-        if state.part:    
-            conditions.append("c{i}.stem = {stem}".format(i = str(i), stem = state.part == 'STEM'))
-        if state.type:    
-            conditions.append("(c{i}.properties).type = '{type}'".format(i = str(i), type = state.type))
+            # Characters
+            if node.data == 'value':
+                if len(node.children) == 2:
+                    plpy.notice(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN value_variants b USING (value_id) WHERE a.value = '{node.children[0]}' AND b.value = '{node.children[1].lower()}'")
+                    r = plpy.execute(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN values x USING (value_id) JOIN values y USING (sign_id) JOIN value_variants b ON (y.value_id = b.value_id) WHERE a.value = '{node.children[0]}' AND b.value = '{node.children[1].lower()}'")
+                else:
+                    r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants WHERE value = '{node.children[0]}'")
+                value_ids = r[0]['ids']
+                if not value_ids:
+                    raise ValueError
+                if len(value_ids) == 1:
+                    self.conditions.append(f"c{self.i}.value_id = {value_ids[0]}")
+                else:
+                    self.conditions.append(f"c{self.i}.value_id = ANY (ARRAY{value_ids})")
+                     
+                self.commit(False)
+            elif node.data == 'sign':
+                r = plpy.execute(f"SELECT array_agg(component_sign_id order by pos) AS ids FROM value_variants JOIN values USING (value_id) JOIN sign_composition USING (sign_id) WHERE value = '{node.children[0].lower()}'")
+                component_sign_ids = r[0]['ids']
+                if not component_sign_ids:
+                    raise ValueError
+                for component_sign_id in component_sign_ids:
+                    self.conditions.append(f"c{self.i}.component_sign_id = {component_sign_id}")
+                    self.commit(True)
+            elif node.data == 'pattern':
+                pattern = '^'+node.children[0][1:-1].replace('_', '.')+'([0-9]+|x)?$'
+                if len(node.children) == 2:
+                    r = plpy.execute(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN values x USING (value_id) JOIN values y USING (sign_id) JOIN value_variants b ON (y.value_id = b.value_id) WHERE a.value ~ '{pattern}' AND b.value = '{node.children[1].lower()}'")
+                else:
+                    r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants WHERE value ~ '{pattern}'")
+                value_ids = r[0]['ids']
+                if not value_ids:
+                    raise ValueError 
+                if len(value_ids) == 1:
+                    self.conditions.append(f"c{self.i}.value_id = {value_ids[0]}")
+                else:
+                    self.conditions.append(f"c{self.i}.value_id = ANY (ARRAY{value_ids})")
+                    
+                self.commit(False)
+            elif node.data == 'signx':
+                self.commit(True)
+            elif node.data == 'valuex':
+                if len(node.children):
+                    r = plpy.execute(f"SELECT sign_id FROM value_variants JOIN values USING (value_id) WHERE value = '{node.children[0].lower()}'")
+                    if not len(r):
+                        raise ValueError
+                    self.conditions.append(f"c{self.i}.sign_id = {r[0]['sign_id']}")
+                self.commit(False)
+            elif node.data == 'n':
+                self.conditions.append(f"(c{self.i}.properties).type = 'number'")
+                self.commit(False)
 
-        i += 1
+            elif node.data == 'line':
+                self.sameLineNext = True
+                for child in node.children:
+                    self.visit(child)
+            elif node.data == 'word':
+                self.sameWordNext = True
+                for child in node.children:
+                    self.visit(child)
+                self.sameWord = None
 
+            elif node.data == 'lindicator':
+                self.alignment = 'right'
+                for child in node.children:
+                    self.visit(child)
+                self.alignment = None
+            elif node.data == 'rindicator':
+                self.alignment = 'left'
+                for child in node.children:
+                    self.visit(child)
+                self.alignment = None
+            elif node.data == 'indicator':
+                self.indicator = True
+                for child in node.children:
+                    self.visit(child)
+                self.indicator = None
+            elif node.data == 'det':
+                self.phonographic = False
+                for child in node.children:
+                    self.visit(child)
+                self.phonographic = None
+            elif node.data == 'pc':
+                self.phonographic = True
+                for child in node.children:
+                    self.visit(child)
+                self.phonographic = None
 
-    from_clause = ' JOIN '.join([from_clause] + [target_table+' '+join_clause('c0', 'c'+str(j)) for j in range(1,i)] + cte_joins)
+            elif node.data == 'dots':
+                self.consecutive = False
+            elif node.data == 'wordbreak':
+                self.sameWord = False
+            elif node.data == 'linebreak':
+                self.sameLine = False
 
-    if tokens[-1] in ';›':
-        from_clause += ' JOIN {table} {join_clause1} JOIN (SELECT {key}, max(sign_no) as max_sign_no FROM {table} GROUP BY {key}) {join_clause2}'.format(key=', '.join(target_key), join_clause1=join_clause('c0', 'c_end'), join_clause2=join_clause('c0', 'counts'), table=target_table)
-        if tokens[-1] == ';':
-            conditions.append('((c_end.sign_no = c{i}.sign_no + 1 AND c{i}.word_no != c_end.word_no) OR (c_end.sign_no = c{i}.sign_no AND c_end.sign_no = max_sign_no))'.format(i = str(i-1)))
-        elif tokens[-1] == '›':
-            conditions.append("((c_end.sign_no = c{i}.sign_no + 1 AND c_end.segment_type = 'SUFFIX') OR (c_end.sign_no = c{i}.sign_no AND c_end.sign_no = max_sign_no))".format(i = str(i-1)))
+            else:
+                for child in node.children:
+                    self.visit(child)
 
-    array_clause = ', '.join(['c{j}.sign_no'.format(j = str(j)) for j in range(i)])
-    where_clause = ' AND '.join(conditions) if conditions else 'TRUE'
-    cte_clause = ', '.join(ctes)
+    tree = l.parse(search_term)
+    r = Listener()
+    r.visit(tree)
+    plpy.notice(r.compose())
+    return r.compose()
 
-    return 'WITH {cte_clause} SELECT {keys}, ARRAY[{array_clause}] AS signs FROM {from_clause} WHERE {where_clause}'.format(keys = ', '.join(['c0.'+key for key in target_key]), cte_clause = cte_clause, array_clause = array_clause, from_clause = from_clause, where_clause = where_clause)
+print(parse_search('X x', 'corpus', ['text_id']))
