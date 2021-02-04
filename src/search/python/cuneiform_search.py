2eeb0b9ff23a37559c6ab4c0fe32496156e4dd23
Marc Endesfelder <marc.endesfelder@gmail.com>
2021-02-04 15:33:09 -0500

    Implement start and end breaks;  better sign identification

--- src/search/python/cuneiform_search.py
+++ src/search/python/cuneiform_search.py
@@ -7,7 +7,7 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
      
     from lark import Lark
     grammar = r"""
-    start: (line|word) (_space (line|word))*
+    start: breakstart? (line|word) (_space (line|word))* breakend?
     line: "[" word (_nbspace word)* "]"
     word: _part (_sep _part)*
     _sep: dots?_SEP(dots _SEP?)?
@@ -20,27 +20,34 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
     det: "{" char (_sep char)* "}"
     pc: "<" char (_sep char)* ">"
     char: VALUE _signspec?   -> value
-        | SIGN              -> sign
+        | SIGN               -> sign
         | PATTERN _signspec? -> pattern
-        | _SIGNX            -> signx
+        | _SIGNX             -> signx
         | _VALUEX _signspec? -> valuex
-        | _N                -> n
-    _signspec: "(" SIGN ")"
+        | _N                 -> n
+    _signspec: COMMENT
+
+    breakstart: (wordbreak|linebreak)+
+    breakend: (wordbreak|linebreak)+
 
     dots: _DOTS
     wordbreak: ";"
     linebreak: _NL
 
-    SIGN.3: /[ABDEGĜHḪIJKLMNPQRSŠṢTṬUWYZ’]+[0-9]*/
-    VALUE.2: /[abdegĝhḫijklmnpqrsšṣtṭuwyz’]+[0-9xX]*/
-    _SIGNX.1: "X"
-    _VALUEX.1: "x"
-    _N.1: /[Nn]/
-    PATTERN.0: /\/[^\/]*\//
-    _DOTS.1: "…"|"..."
-    _SEP.0: /[.-]/
+    COMMENT.5: "(" _DOTSIGN ")"
+    PATTERN.5: /\/[^\/]*\//
+    SIGN.4: _SIMPLESIGN (_SIGNCON _SIMPLESIGN)*
+    VALUE.3: /[abdegĝhḫijklmnpqrsšṣtṭuwyz’]+[0-9xX]*/
+    _SIGNX.2: "X"
+    _VALUEX.2: "x"
+    _N.2: /[Nn]/
+    _DOTS.2: "…"|"..."
+    _SEP.1: /[.-]/
     _SPACE: /[\t \f\n]+/
     _NL: "//"
+    _SIGNCON: /[×x%&]/
+    _SIMPLESIGN: /[ABDEGĜHḪIJKLMNPQRSŠṢTṬUWYZ’]+[0-9]*/
+    _DOTSIGN: _SIMPLESIGN ((_SIGNCON|".") _SIMPLESIGN)*
     """
     l = Lark(grammar, lexer='standard')
 
@@ -57,6 +64,10 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
             self.phonographic = None
 
             self.previous_sign = False
+
+            self.breakStart = False
+            self.linebreakEnd = False
+            self.wordbreakEnd = False
 
             self.i = 0
             self.conditions = []
@@ -118,19 +129,23 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
             self.previous_sign = sign
 
         def compose(self):
-            fromClause = ', '.join(self.sources)
+            fromClause = ' CROSS JOIN '.join(self.sources)
             whereClause = ' AND '.join(self.conditions)
             arrayClause = ', '.join(f'c{i}.sign_no' for i in range(self.i))
+            frontKey = ' AND '.join([f'cstart.{key} = c0.{key}' for key in target_key])
+            backKey = ' AND '.join([f'cend.{key} = c0.{key}' for key in target_key])
+            frontJoinClause = f'LEFT JOIN corpus cstart ON cstart.sign_no = c0.sign_no-1 AND {frontKey}'
+            backJoinClause = f'LEFT JOIN corpus cend ON cend.sign_no = c{self.i-1}.sign_no+1 AND {backKey}'
             keys = ', '.join([f'c0.{key}' for key in target_key])
-            return f'SELECT {keys}, ARRAY[{arrayClause}] AS signs FROM {fromClause} WHERE {whereClause};'
+            return f'SELECT {keys}, ARRAY[{arrayClause}] AS signs FROM {fromClause} {frontJoinClause} {backJoinClause} WHERE {whereClause}'
         
         def visit(self, node):
 
             # Characters
             if node.data == 'value':
                 if len(node.children) == 2:
-                    plpy.notice(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN value_variants b USING (value_id) WHERE a.value = '{node.children[0]}' AND b.value = '{node.children[1].lower()}'")
-                    r = plpy.execute(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN values x USING (value_id) JOIN values y USING (sign_id) JOIN value_variants b ON (y.value_id = b.value_id) WHERE a.value = '{node.children[0]}' AND b.value = '{node.children[1].lower()}'")
+                    #r = plpy.execute(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN values x USING (value_id) JOIN values y USING (sign_id) JOIN value_variants b ON (y.value_id = b.value_id) WHERE a.value = '{node.children[0]}' AND b.value = '{node.children[1].lower()}'")
+                    r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants JOIN values USING (value_id) JOIN sign_identifiers USING (sign_id) WHERE value = '{node.children[0]}' AND sign_identifier = '{node.children[1][1:-1].replace('x', '×')}'")
                 else:
                     r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants WHERE value = '{node.children[0]}'")
                 value_ids = r[0]['ids']
@@ -139,11 +154,10 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
                 if len(value_ids) == 1:
                     self.conditions.append(f"c{self.i}.value_id = {value_ids[0]}")
                 else:
-                    self.conditions.append(f"c{self.i}.value_id = ANY (ARRAY{value_ids})")
-                     
+                    self.conditions.append(f"c{self.i}.value_id = ANY (ARRAY{value_ids})")          
                 self.commit(False)
             elif node.data == 'sign':
-                r = plpy.execute(f"SELECT array_agg(component_sign_id order by pos) AS ids FROM value_variants JOIN values USING (value_id) JOIN sign_composition USING (sign_id) WHERE value = '{node.children[0].lower()}'")
+                r = plpy.execute(f"SELECT array_agg(component_sign_id order by pos) AS ids FROM sign_identifiers JOIN sign_composition USING (sign_id) WHERE sign_identifier = '{node.children[0]}'")
                 component_sign_ids = r[0]['ids']
                 if not component_sign_ids:
                     raise ValueError
@@ -151,9 +165,9 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
                     self.conditions.append(f"c{self.i}.component_sign_id = {component_sign_id}")
                     self.commit(True)
             elif node.data == 'pattern':
-                pattern = '^'+node.children[0][1:-1].replace('_', '.')+'([0-9]+|x)?$'
+                pattern = '^'+node.children[0][1:-1]+'([0-9]+|x)?$'
                 if len(node.children) == 2:
-                    r = plpy.execute(f"SELECT array_agg(a.value_id) AS ids FROM value_variants a JOIN values x USING (value_id) JOIN values y USING (sign_id) JOIN value_variants b ON (y.value_id = b.value_id) WHERE a.value ~ '{pattern}' AND b.value = '{node.children[1].lower()}'")
+                    r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants JOIN values USING (value_id) JOIN sign_identifiers USING (sign_id) WHERE value = '{pattern}' AND sign_identifier = '{node.children[1][1:-1].replace('x', '×')}'")
                 else:
                     r = plpy.execute(f"SELECT array_agg(value_id) AS ids FROM value_variants WHERE value ~ '{pattern}'")
                 value_ids = r[0]['ids']
@@ -169,7 +183,7 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
                 self.commit(True)
             elif node.data == 'valuex':
                 if len(node.children):
-                    r = plpy.execute(f"SELECT sign_id FROM value_variants JOIN values USING (value_id) WHERE value = '{node.children[0].lower()}'")
+                    r = plpy.execute(f"SELECT sign_id FROM sign_identifiers WHERE sign_identifier = '{node.children[0][1:-1].replace('x', '×')}'")
                     if not len(r):
                         raise ValueError
                     self.conditions.append(f"c{self.i}.sign_id = {r[0]['sign_id']}")
@@ -221,14 +235,34 @@ def parse_search(search_term:str, target_table:str, target_key:List[str]) -> str
             elif node.data == 'linebreak':
                 self.sameLine = False
 
+            elif node.data == 'breakstart':
+                self.breakStart = True
+                for child in node.children:
+                    if child.data == 'wordbreak':
+                        self.conditions.append(f"(cstart.word_no IS NULL OR cstart.word_no != c0.word_no)")
+                    elif child.data == 'linebreak':
+                        self.conditions.append(f"(cstart.line_no IS NULL OR cstart.lineno != c0.line_no)")
+            elif node.data == 'breakend':
+                if self.previous_sign:
+                    self.conditions.append(f'c{self.i-1}.final')
+                for child in node.children:
+                    if child.data == 'wordbreak':
+                        self.conditions.append(f"(cend.word_no IS NULL OR cend.word_no != c{self.i-1}.word_no)")
+                    elif child.data == 'linebreak':
+                        self.conditions.append(f"(cend.line_no IS NULL OR cend.line_no != c{self.i-1}.word_no)") 
+
             else:
                 for child in node.children:
                     self.visit(child)
 
-    tree = l.parse(search_term)
-    r = Listener()
-    r.visit(tree)
+    try:
+        tree = l.parse(search_term.translate(search_term.maketrans('cjvCJV', 'šĝřŠĜŘ')))
+        r = Listener()
+        r.visit(tree)
+    except:
+        keys = ', '.join(target_key)
+        return f'SELECT {keys}, ARRAY[]::integer[] AS signs FROM {target_table} WHERE FALSE'
     plpy.notice(r.compose())
     return r.compose()
 
-print(parse_search('X x', 'corpus', ['text_id']))
+print(parse_search('x(A.A)', 'corpus', ['text_id']))
