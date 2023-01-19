ca09069c6513ea932d5d4051be6f8f680d3ae971
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-01-19 16:43:38 +0100

    Fix citation bug; replace materialized views with triggers

--- src/citation/sql/cuneiform_citation.sql
+++ src/citation/sql/cuneiform_citation.sql
@@ -48,43 +48,65 @@ CREATE OR REPLACE FUNCTION romanize_column (col text)
     return re.sub('[0-9]+', lambda m: str(roman.toRoman(int(m.group(0)))).lower(), col)
 $BODY$;
 
-CREATE MATERIALIZED VIEW blocks_display AS
+
+CREATE TABLE blocks_display (
+    transliteration_id integer,
+    block_no integer,
+    short text,
+    long text,
+    PRIMARY KEY (transliteration_id, block_no)
+);
+
+CREATE TABLE surfaces_display (
+    transliteration_id integer,
+    surface_no integer,
+    short text,
+    long text,
+    PRIMARY KEY (transliteration_id, surface_no)
+);
+
+CREATE TABLE objects_display (
+    transliteration_id integer,
+    object_no integer,
+    name text,
+    sort_order integer,
+    PRIMARY KEY (transliteration_id, object_no)
+);
+
+
+CREATE VIEW blocks_display_view AS
 SELECT
     transliteration_id,
     block_no,
-    coalesce (
-        CASE block_type
+    CASE block_type
         WHEN 'column' THEN
             romanize_column (block_data)
         WHEN 'summary' THEN
             romanize_column (block_data)
         WHEN 'block' THEN
-            block_data
+            COALESCE(block_data, '')
         ELSE
-            block_type || ' ' || block_data
-        END, '') AS short,
-    coalesce (
-        CASE block_type
+            block_type || COALESCE(' ' || block_data, '')
+    END AS short,
+    CASE block_type
         WHEN 'column' THEN
             romanize_column (block_data)
         WHEN 'summary' THEN
             romanize_column (block_data)
         WHEN 'block' THEN
-            block_data
+            COALESCE(block_data, '')
         ELSE
-            block_type || ' ' || block_data
-        END, '') || coalesce(block_comment, '') AS long
+            block_type || COALESCE(' ' || block_data, '')
+    END || COALESCE(block_comment, '') AS long
 FROM
     blocks;
 
-CREATE UNIQUE INDEX ON blocks_display (transliteration_id, block_no);
 
-CREATE MATERIALIZED VIEW surfaces_display AS
+CREATE VIEW surfaces_display_view AS
 SELECT
     transliteration_id,
     surface_no,
-    coalesce (
-        CASE surface_type
+    CASE surface_type
         WHEN 'obverse' THEN
             'o.'
         WHEN 'reverse' THEN
@@ -97,11 +119,12 @@ SELECT
             'l.e.'
         WHEN 'right' THEN
             'r.e.'
+        WHEN 'surface' THEN
+            COALESCE(surface_data, '')
         ELSE
-            surface_data
-        END, '') AS short,
-    coalesce (
-        CASE surface_type
+            surface_type || COALESCE(' ' || surface_data, '')
+    END AS short,
+    CASE surface_type
         WHEN 'obverse' THEN
             'obverse'
         WHEN 'reverse' THEN
@@ -114,15 +137,16 @@ SELECT
             'left edge'
         WHEN 'right' THEN
             'right edge'
+        WHEN 'surface' THEN
+            COALESCE(surface_data, '')
         ELSE
-            surface_data
-        END, '') || coalesce(surface_comment, '') AS long
+            surface_type || COALESCE(' ' || surface_data, '')
+    END || COALESCE(surface_comment, '') AS long
 FROM
     surfaces;
 
-CREATE UNIQUE INDEX ON surfaces_display (transliteration_id, surface_no);
 
-CREATE MATERIALIZED VIEW objects_display AS
+CREATE VIEW objects_display_view AS
 SELECT
     transliteration_id,
     object_no,
@@ -146,4 +170,107 @@ SELECT
 FROM
     objects;
 
-CREATE UNIQUE INDEX ON objects_display (transliteration_id, object_no);
+
+CREATE FUNCTION blocks_display_trigger_fun () 
+  RETURNS trigger 
+  VOLATILE
+  LANGUAGE PLPGSQL
+  AS
+$BODY$
+BEGIN
+    IF NEW IS NULL THEN
+        DELETE FROM blocks_display WHERE transliteration_id = (OLD).transliteration_id AND block_no = (OLD).block_no;
+    ELSIF OLD IS NULL THEN
+        INSERT INTO blocks_display SELECT * FROM blocks_display_view WHERE transliteration_id = (NEW).transliteration_id AND block_no = (NEW).block_no;
+    ELSE
+        UPDATE blocks_display SET 
+            transliteration_id = (NEW).transliteration_id, 
+            block_no = (NEW).block_no, 
+            short = x.short, 
+            long = x.long 
+        FROM blocks_display_view x
+        WHERE 
+            blocks_display.transliteration_id = (OLD).transliteration_id
+            AND blocks_display.block_no = (OLD).block_no
+            AND x.transliteration_id = (NEW).transliteration_id
+            AND x.block_no = (NEW).block_no;
+    END IF;
+    RETURN NULL;
+END;
+$BODY$;
+
+CREATE FUNCTION surfaces_display_trigger_fun () 
+  RETURNS trigger 
+  VOLATILE
+  LANGUAGE PLPGSQL
+  AS
+$BODY$
+BEGIN
+    IF NEW IS NULL THEN
+        DELETE FROM surfaces_display WHERE transliteration_id = (OLD).transliteration_id AND surface_no = (OLD).surface_no;
+    ELSIF OLD IS NULL THEN
+        INSERT INTO surfaces_display SELECT * FROM surfaces_display_view WHERE transliteration_id = (NEW).transliteration_id AND surface_no = (NEW).surface_no;
+    ELSE
+        UPDATE surfaces_display SET 
+            transliteration_id = (NEW).transliteration_id, 
+            surface_no = (NEW).surface_no, 
+            short = x.short, 
+            long = x.long 
+        FROM surfaces_display_view x
+        WHERE 
+            surfaces_display.transliteration_id = (OLD).transliteration_id
+            AND surfaces_display.surface_no = (OLD).surface_no
+            AND x.transliteration_id = (NEW).transliteration_id
+            AND x.surface_no = (NEW).surface_no;
+    END IF;
+    RETURN NULL;
+END;
+$BODY$;
+
+CREATE FUNCTION objects_display_trigger_fun () 
+  RETURNS trigger 
+  VOLATILE
+  LANGUAGE PLPGSQL
+  AS
+$BODY$
+BEGIN
+    IF NEW IS NULL THEN
+        DELETE FROM objects_display WHERE transliteration_id = (OLD).transliteration_id AND object_no = (OLD).object_no;
+    ELSIF OLD IS NULL THEN
+        INSERT INTO objects_display SELECT * FROM objects_display_view WHERE transliteration_id = (NEW).transliteration_id AND object_no = (NEW).object_no;
+    ELSE
+        UPDATE objects_display SET 
+            transliteration_id = (NEW).transliteration_id, 
+            object_no = (NEW).object_no, 
+            name = x.name, 
+            sort_order = x.sort_order 
+        FROM objects_display_view x
+        WHERE 
+            objects_display.transliteration_id = (OLD).transliteration_id
+            AND objects_display.object_no = (OLD).object_no
+            AND x.transliteration_id = (NEW).transliteration_id
+            AND x.object_no = (NEW).object_no;
+    END IF;
+    RETURN NULL;
+END;
+$BODY$;
+
+
+CREATE TRIGGER blocks_display_trigger
+  AFTER INSERT OR DELETE OR UPDATE ON blocks 
+  FOR EACH ROW
+  EXECUTE FUNCTION blocks_display_trigger_fun();
+
+CREATE TRIGGER surfaces_display_trigger
+  AFTER INSERT OR DELETE OR UPDATE ON surfaces 
+  FOR EACH ROW
+  EXECUTE FUNCTION surfaces_display_trigger_fun();
+
+CREATE TRIGGER objects_display_trigger
+  AFTER INSERT OR DELETE OR UPDATE ON objects 
+  FOR EACH ROW
+  EXECUTE FUNCTION objects_display_trigger_fun();
+
+INSERT INTO blocks_display SELECT * FROM blocks_display_view;
+INSERT INTO surfaces_display SELECT * FROM surfaces_display_view;
+INSERT INTO objects_display SELECT * FROM objects_display_view;
