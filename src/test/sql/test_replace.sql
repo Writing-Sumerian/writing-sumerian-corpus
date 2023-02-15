c0fd6221028c3b51ed9fca56edcde06723e470ff
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-02-15 10:03:07 +0100

    Update tests

--- src/test/sql/test_replace.sql
+++ src/test/sql/test_replace.sql
@@ -6,17 +6,17 @@ CREATE OR REPLACE FUNCTION test_replace_basic ()
 $BODY$
 BEGIN
 CALL parse(E'1\ti3 ni', 'public', 'sumerian', false, -1);
-CALL replace('i3','a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('i3','a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 ni') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('i3','ni','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('i3','ni','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tni ni') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('NI','i3','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('NI','i3','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 i3') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('NI NI','i3 NI','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('NI NI','i3 NI','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 NI') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('NI NI','i3','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('NI NI','i3','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 NI') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('NI NI','i3 NI e','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('NI NI','i3 NI e','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 NI') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -29,13 +29,13 @@ CREATE OR REPLACE FUNCTION test_replace_basic_references ()
 $BODY$
 BEGIN
 CALL parse(E'1\ti3 ni', 'public', 'sumerian', false, -1);
-CALL replace('(NI) (NI)','"2" "1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(NI) (NI)','"2" "1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tni i3') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(NI NI)','i3 "1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(NI NI)','i3 "1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tni i3') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(NI) (NI)','"2"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(NI) (NI)','"2"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tni i3') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(NI) (NI)','"2" "2"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(NI) (NI)','"2" "2"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ti3 i3') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -48,11 +48,11 @@ CREATE OR REPLACE FUNCTION test_replace_composites ()
 $BODY$
 BEGIN
 CALL parse(E'1\tensi2 a diri', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','si-a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','si-a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tensi2 a si-a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tensi2 a diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('ENSI2 a','PA.TE diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('ENSI2 a','PA.TE diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tPA.TE diri diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -66,9 +66,9 @@ CREATE OR REPLACE FUNCTION test_replace_overlap ()
 $BODY$
 BEGIN
 CALL parse(E'1\ta a a', 'public', 'sumerian', false, -1);
-CALL replace('A A','ayya','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('A A','ayya','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ta a a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(A) A A','"1" aya','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(A) A A','"1" aya','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\ta aya') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -82,13 +82,13 @@ CREATE OR REPLACE FUNCTION test_replace_composite_references ()
 $BODY$
 BEGIN
 CALL parse(E'1\tdiri si-a', 'public', 'sumerian', false, -1);
-CALL replace('(DIRI) … (DIRI)','"2"-"1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','"2"-"1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi-a-diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(DIRI) … (DIRI)','si a si','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','si a si','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi-a-diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(DIRI) … (DIRI)','si a si a si','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','si a si a si','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi-a-diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(DIRI) … (DIRI)','si a si a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','si a si a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi a si a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -101,11 +101,11 @@ CREATE OR REPLACE FUNCTION test_replace_gap ()
 $BODY$
 BEGIN
 CALL parse(E'1\tdiri e si-a', 'public', 'sumerian', false, -1);
-CALL replace('(DIRI) … (DIRI)','"2"-"1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','"2"-"1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tdiri e si-a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(DIRI) … (DIRI)','"2"--"1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','"2"--"1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tdiri e si-a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('(DIRI) … (DIRI)','"2" "1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) … (DIRI)','"2" "1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi-a e diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -118,9 +118,9 @@ CREATE OR REPLACE FUNCTION test_replace_ligature ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi+a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi+a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('si','sig9','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('si','sig9','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsig9+a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -133,9 +133,9 @@ CREATE OR REPLACE FUNCTION test_replace_inversion ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi:a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi:a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('si','sig9','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('si','sig9','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsig9:a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -148,9 +148,9 @@ CREATE OR REPLACE FUNCTION test_replace_ligature ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi+a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi+a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('si','sig9','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('si','sig9','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsig9+a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -163,9 +163,9 @@ CREATE OR REPLACE FUNCTION test_replace_word ()
 $BODY$
 BEGIN
 CALL parse(E'1\te-si &a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te-diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('DIRI','si &a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','si &a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te-si &a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -178,7 +178,7 @@ CREATE OR REPLACE FUNCTION test_replace_word_reference ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi-a-si a', 'public', 'sumerian', false, -1);
-CALL replace('(DIRI) (DIRI)','"2" "1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) (DIRI)','"2" "1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi a si-a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -191,9 +191,9 @@ CREATE OR REPLACE FUNCTION test_replace_compound_1 ()
 $BODY$
 BEGIN
 CALL parse(E'1\te--si %person &a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te--diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('DIRI','si %person &a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','si %person &a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te--si %person &a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -206,9 +206,9 @@ CREATE OR REPLACE FUNCTION test_replace_compound_2 ()
 $BODY$
 BEGIN
 CALL parse(E'1\te--si %a a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te--diri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('DIRI','si %a a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','si %a a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\te--si %a a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -221,7 +221,7 @@ CREATE OR REPLACE FUNCTION test_replace_compound_reference ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi--a--si a', 'public', 'sumerian', false, -1);
-CALL replace('(DIRI) (DIRI)','"2" "1"','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('(DIRI) (DIRI)','"2" "1"','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi a si--a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -234,7 +234,7 @@ CREATE OR REPLACE FUNCTION test_replace_compound_comment_1 ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi (?) a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi (?) a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -247,7 +247,7 @@ CREATE OR REPLACE FUNCTION test_replace_compound_comment_2 ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi a (?)', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi a (?)') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
@@ -260,9 +260,22 @@ CREATE OR REPLACE FUNCTION test_replace_capitalization ()
 $BODY$
 BEGIN
 CALL parse(E'1\tsi &a', 'public', 'sumerian', false, -1);
-CALL replace('DIRI','diri','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('DIRI','diri','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tdiri') FROM corpus_code_transliterations WHERE transliteration_id = -1;
-CALL replace('diri','si &a','sumerian', false, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+CALL replace('diri','si &a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
 RETURN NEXT is(content, E'1\tsi &a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION test_replace_multiple ()
+    RETURNS SETOF text
+    VOLATILE
+    LANGUAGE PLPGSQL
+    AS 
+$BODY$
+BEGIN
+CALL parse(E'1\tdiri diri', 'public', 'sumerian', false, -1);
+CALL replace('diri','si-a','sumerian', false, -1, true, ARRAY[]::integer[], ARRAY[]::integer[], ARRAY[-1]);
+RETURN NEXT is(content, E'1\tsi-a si-a') FROM corpus_code_transliterations WHERE transliteration_id = -1;
 END;
 $BODY$;
