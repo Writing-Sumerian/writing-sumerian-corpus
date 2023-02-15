4c65622840f036b5691b85249ec68dae29d037a6
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-02-15 10:02:53 +0100

    Adjust replace to changes, fix bug replacing multiple matches in transliteration

--- src/replace/sql/corpus_replace.sql
+++ src/replace/sql/corpus_replace.sql
@@ -1,10 +1,11 @@
-
 CREATE OR REPLACE FUNCTION corpus_replace (
         v_transliteration_id integer,
         v_pattern_id integer,
         v_sign_nos integer[],
+        v_match_nos integer[],
         v_reference_ids integer[],
-        v_reference_sign_nos integer[]
+        v_reference_sign_nos integer[],
+        v_reference_match_nos integer[]
     )
     RETURNS SETOF replace.corpus
     STABLE
@@ -13,26 +14,35 @@ CREATE OR REPLACE FUNCTION corpus_replace (
     AS
 $BODY$
 
-WITH "references" AS (
+WITH matched_signs AS (
     SELECT 
+        match_no,
+        sign_no
+    FROM
+        UNNEST(v_match_nos, v_sign_nos) AS _(match_no, sign_no)
+),
+"references" AS (
+    SELECT 
+        match_no,
         reference_id,
         sign_no
     FROM
-        UNNEST(v_reference_ids, v_reference_sign_nos) AS _(reference_id, sign_no)
+        UNNEST(v_reference_match_nos, v_reference_ids, v_reference_sign_nos) AS _(match_no, reference_id, sign_no)
 ),
 original AS (
     SELECT
-        *,
-        sign_no = ANY(v_sign_nos) AS matched
+        *
     FROM
         corpus
         LEFT JOIN words USING (transliteration_id, word_no)
+        LEFT JOIN matched_signs USING (sign_no)
     WHERE
         transliteration_id = v_transliteration_id
 ),
 replacement_ AS (
     SELECT
-        row_number() OVER (ORDER BY a.sign_no, b.sign_no) - 1 AS sign_no,
+        match.match_no,
+        row_number() OVER (PARTITION BY match.match_no ORDER BY a.sign_no, b.sign_no) - 1 AS sign_no,
         COALESCE(b.value_id, a.value_id) AS value_id,
         COALESCE(b.sign_variant_id, a.sign_variant_id) AS sign_variant_id,
         COALESCE(b.custom_value, CASE WHEN reference_id IS NULL THEN a.custom_value ELSE NULL END) AS custom_value,
@@ -57,9 +67,15 @@ replacement_ AS (
         a_words.compound_no AS compound_no_pattern,
         b.compound_no AS compound_no_reference
     FROM
-        replace.corpus_pattern a
+        (SELECT DISTINCT match_no FROM matched_signs) match
+        CROSS JOIN replace.corpus_pattern a
         LEFT JOIN replace.words_pattern a_words USING (pattern_id, word_no)
-        LEFT JOIN "references" ON (type = 'description' AND custom_value ~ '^[0-9]+' AND reference_id = custom_value::integer)
+        LEFT JOIN "references" ON (
+            type = 'description' 
+            AND custom_value ~ '^[0-9]+' 
+            AND reference_id = custom_value::integer 
+            AND "references".match_no = match.match_no
+        )
         LEFT JOIN original b ON (b.sign_no = "references".sign_no)
     WHERE
         a.pattern_id = v_pattern_id
@@ -69,6 +85,7 @@ replacement_ AS (
 ),
 replacement AS (
     SELECT
+        match_no,
         v_transliteration_id AS transliteration_id,
         sign_no,
         sum(new_word::integer) OVER (ORDER BY sign_no) AS word_no,
@@ -95,7 +112,8 @@ replacement AS (
 ),
 components_original AS (
     SELECT 
-        row_number() OVER (ORDER BY sign_no, ord) AS component_no,
+        match_no,
+        row_number() OVER (PARTITION BY match_no ORDER BY sign_no, ord) AS component_no,
         sign_no,
         glyph_id
     FROM
@@ -103,11 +121,12 @@ components_original AS (
         LEFT JOIN sign_variants_composition USING (sign_variant_id)
         LEFT JOIN LATERAL UNNEST (glyph_ids) WITH ORDINALITY AS _(glyph_id, ord) ON TRUE
     WHERE
-        matched
+        match_no IS NOT NULL
 ),
 components_replacement AS (
     SELECT 
-        row_number() OVER (ORDER BY sign_no, ord) AS component_no,
+        match_no,
+        row_number() OVER (PARTITION BY match_no ORDER BY sign_no, ord) AS component_no,
         sign_no,
         glyph_id
     FROM
@@ -119,13 +138,14 @@ correspondence AS (
     SELECT DISTINCT
         a.sign_no AS sign_no_old,
         b.sign_no AS sign_no_new,
-        COALESCE(a.sign_no - lag(a.sign_no) OVER (PARTITION BY b.sign_no ORDER BY a.sign_no), 0) AS gap
+        COALESCE(a.sign_no - lag(a.sign_no) OVER (PARTITION BY match_no, b.sign_no ORDER BY a.sign_no), 0) AS gap
     FROM
         components_original a
-        FULL JOIN components_replacement b USING (component_no, glyph_id)
+        FULL JOIN components_replacement b USING (match_no, component_no, glyph_id)
 ),
 retained_values AS (
     SELECT
+        match_no,
         sign_no_new AS sign_no,
         min(line_no) AS line_no,
         condition_agg(condition) AS condition,
@@ -145,10 +165,12 @@ retained_values AS (
         original
         JOIN correspondence ON (sign_no = sign_no_old)
     GROUP BY
+        match_no,
         sign_no_new
 ),
 patched_ AS (
     SELECT
+        match_no,
         line_no,
         value_id,
         sign_variant_id,
@@ -176,9 +198,10 @@ patched_ AS (
         valid
     FROM
         replacement
-        FULL JOIN retained_values USING (sign_no)
+        FULL JOIN retained_values USING (match_no, sign_no)
     UNION ALL
     SELECT
+        match_no,
         line_no,
         value_id,
         sign_variant_id,
@@ -207,32 +230,33 @@ patched_ AS (
     FROM
         original
     WHERE
-        NOT matched
+        match_no IS NULL
 ),
 patched AS (
     SELECT
         *,
         row_number() OVER w1 - 1 AS sign_no_final,
         COALESCE(
-            word_no_new != lag(word_no_new) OVER w1, 
+            word_no_new != lag(word_no_new) OVER w2, 
             word_no_old != lag(word_no_old) OVER w1, 
             lag(word_no_old) OVER w1 IS NULL
         ) AS new_word,
         COALESCE(
-            compound_no_new != lag(compound_no_new) OVER w1, 
+            compound_no_new != lag(compound_no_new) OVER w2, 
             compound_no_old != lag(compound_no_old) OVER w1, 
             lag(compound_no_old) OVER w1 IS NULL
         ) AS new_compound,
         valid 
-            AND COALESCE(compound_no_new IS NULL OR compound_no_old <= lag(compound_no_old) OVER w2 + 1, true)
-            AND COALESCE(word_no_new IS NULL OR word_no_old <= lag(word_no_old) OVER w3 + 1, true) 
+            AND COALESCE(compound_no_new IS NULL OR compound_no_old <= lag(compound_no_old) OVER w3 + 1, true)
+            AND COALESCE(word_no_new IS NULL OR word_no_old <= lag(word_no_old) OVER w4 + 1, true) 
             AS valid_final
     FROM
         patched_
     WINDOW 
-        w1 AS (ORDER BY sign_no_old, sign_no_new),
-        w2 AS (PARTITION BY compound_no_new ORDER BY sign_no_old, sign_no_new),
-        w3 AS (PARTITION BY word_no_new ORDER BY sign_no_old, sign_no_new)
+        w1 AS (ORDER BY sign_no_old, match_no, sign_no_new),
+        w2 AS (PARTITION BY match_no ORDER BY sign_no_old, sign_no_new),
+        w3 AS (PARTITION BY match_no, compound_no_new ORDER BY sign_no_old, sign_no_new),
+        w4 AS (PARTITION BY match_no, word_no_new ORDER BY sign_no_old, sign_no_new)
 )
 SELECT
     v_transliteration_id,
--- src/replace/sql/replace.sql
+++ src/replace/sql/replace.sql
@@ -3,6 +3,8 @@ CREATE OR REPLACE PROCEDURE replace (
     pattern text,
     language language,
     stemmed boolean,
+    user_id integer,
+    internal boolean,
     period_ids integer[] DEFAULT ARRAY[]::integer[],
     provenience_ids integer[] DEFAULT ARRAY[]::integer[],
     genre_ids integer[] DEFAULT ARRAY[]::integer[]
@@ -17,8 +19,10 @@ DECLARE
     v_pattern_id integer;
     v_transliteration_id integer;
     v_sign_nos integer[];
+    v_match_nos integer[];
     v_reference_ids integer[];
     v_reference_sign_nos integer[];
+    v_reference_match_nos integer[];
     v_overlap boolean;
     v_invalid_sign_nos integer[];
     v_invalid_compound_nos integer[];
@@ -36,62 +40,68 @@ BEGIN
     FROM
         preparse_search(search_term);
 
-    FOR v_transliteration_id, v_sign_nos, v_reference_ids, v_reference_sign_nos, v_overlap IN 
+    FOR v_transliteration_id, v_sign_nos, v_match_nos, v_reference_ids, v_reference_sign_nos, v_reference_match_nos, v_overlap IN 
         WITH 
         results AS (
             SELECT 
-                row_number() OVER (), 
+                row_number() OVER (PARTITION BY transliteration_id ORDER BY sign_nos[1]) AS match_no, 
                 * 
             FROM 
                 search(v_search_term_norm, period_ids, provenience_ids, genre_ids)
         ),
-        sign_overlap AS (
-            SELECT 
-                transliteration_id, 
-                sign_no, 
-                count(*) > 1 AS overlap 
-            FROM 
-                results
-                LEFT JOIN LATERAL unnest(sign_nos) AS a(sign_no) ON TRUE
-            GROUP BY 
-                transliteration_id, 
-                sign_no
-        ),
-        overlap AS (
+        sign_nos_ AS (
             SELECT
-                row_number,
-                bool_or(overlap) AS overlap
+                transliteration_id,
+                sign_no,
+                match_no,
+                count(*) OVER (PARTITION BY transliteration_id, sign_no) AS count
             FROM
                 results
                 LEFT JOIN LATERAL unnest(sign_nos) AS a(sign_no) ON TRUE
-                LEFT JOIN sign_overlap USING (transliteration_id, sign_no)
+        ),
+        sign_nos AS (
+            SELECT
+                transliteration_id,
+                array_agg(sign_no) AS sign_nos,
+                array_agg(match_no) AS match_nos,
+                max(count) > 1 AS overlap
+            FROM
+                sign_nos_
             GROUP BY
-                row_number
+                transliteration_id
+        ),
+        "references" AS (
+            SELECT 
+                transliteration_id, 
+                array_agg(reference_id ORDER BY reference_id, ord) FILTER (WHERE reference_id IS NOT NULL) AS reference_ids, 
+                array_agg(c.sign_no ORDER BY reference_id, ord) FILTER (WHERE reference_id IS NOT NULL) AS reference_sign_nos,
+                array_agg(match_no ORDER BY reference_id, ord) FILTER (WHERE reference_id IS NOT NULL) AS reference_match_nos
+            FROM    
+                results
+                LEFT JOIN LATERAL unnest(wildcards) AS b ON TRUE
+                LEFT JOIN LATERAL unnest(b.sign_nos) WITH ORDINALITY AS c(sign_no, ord) ON TRUE
+                LEFT JOIN LATERAL unnest(v_wildcards_explicit) WITH ORDINALITY AS _(wildcard_id, reference_id) USING (wildcard_id)
+            GROUP BY
+                transliteration_id
         )
-        SELECT 
-            results.transliteration_id, 
-            results.sign_nos, 
-            array_agg(reference_id ORDER BY reference_id, ord) FILTER (WHERE reference_id IS NOT NULL), 
-            array_agg(c.sign_no ORDER BY reference_id, ord) FILTER (WHERE reference_id IS NOT NULL),
+        SELECT
+            transliteration_id,
+            sign_nos,
+            match_nos,
+            reference_ids,
+            reference_sign_nos,
+            reference_match_nos,
             overlap
-        FROM    
-            results
-            LEFT JOIN overlap USING (row_number)
-            LEFT JOIN LATERAL unnest(wildcards) AS b ON TRUE
-            LEFT JOIN LATERAL unnest(b.sign_nos) WITH ORDINALITY AS c(sign_no, ord) ON TRUE
-            LEFT JOIN LATERAL unnest(v_wildcards_explicit) WITH ORDINALITY AS _(wildcard_id, reference_id) USING (wildcard_id)
-        GROUP BY
-            row_number,
-            overlap,
-            results.transliteration_id, 
-            results.sign_nos
+        FROM
+            sign_nos
+            LEFT JOIN "references" USING (transliteration_id)
     LOOP
         IF v_overlap THEN
             RAISE NOTICE 'Skipping %, %: Overlapping match', v_transliteration_id, v_sign_nos;
             CONTINUE;
         END IF;
         BEGIN
-            INSERT INTO replace.corpus SELECT * FROM corpus_replace(v_transliteration_id, v_pattern_id, v_sign_nos, v_reference_ids, v_reference_sign_nos);
+            INSERT INTO replace.corpus SELECT * FROM corpus_replace(v_transliteration_id, v_pattern_id, v_sign_nos, v_match_nos, v_reference_ids, v_reference_sign_nos, v_reference_match_nos);
             INSERT INTO replace.words SELECT * FROM words_replace WHERE transliteration_id = v_transliteration_id;
             INSERT INTO replace.compounds SELECT * FROM compounds_replace WHERE transliteration_id = v_transliteration_id;
             
@@ -107,7 +117,7 @@ BEGIN
             ELSIF v_invalid_compound_nos IS NOT NULL THEN
                 RAISE NOTICE 'Skipping %, %: Cannot remove compounds %', v_transliteration_id, v_sign_nos, v_invalid_compound_nos;
             ELSE
-                PERFORM edit('replace', v_transliteration_id);
+                CALL edit_logged('replace', v_transliteration_id, user_id, internal);
                 RAISE NOTICE 'Replacing signs % in %', v_sign_nos, v_transliteration_id;
             END IF;
 
