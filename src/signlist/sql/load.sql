98b2164a9580a135373507061a2325dbe57076ab
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-03-05 22:42:04 +0100

    Export allograph_ids and sign_variant_ids as well when saving signlist

--- src/signlist/sql/load.sql
+++ src/signlist/sql/load.sql
@@ -14,13 +14,15 @@ EXECUTE format('COPY glyphs(glyph_id, glyph, unicode) FROM %L CSV NULL ''\N''', 
 EXECUTE format('COPY glyph_synonyms(synonym, glyph_id) FROM %L CSV NULL ''\N''', path || 'glyph_synonyms.csv');
 EXECUTE format('COPY glyph_values(value, value_id, glyph_ids) FROM %L CSV NULL ''\N''', path || 'glyph_values.csv');
 EXECUTE format('COPY graphemes(grapheme_id, grapheme, mzl_no) FROM %L CSV NULL ''\N''', path || 'graphemes.csv');
-EXECUTE format('COPY allographs(grapheme_id, glyph_id, variant_type, specific) FROM %L CSV NULL ''\N''', path || 'allographs.csv');
+EXECUTE format('COPY allographs(allograph_id, grapheme_id, glyph_id, variant_type, specific) FROM %L CSV NULL ''\N''', path || 'allographs.csv');
 EXECUTE format('COPY allomorphs(allomorph_id, sign_id, variant_type, specific) FROM %L CSV NULL ''\N''', path || 'allomorphs.csv');
 EXECUTE format('COPY allomorph_components(allomorph_id, pos, grapheme_id) FROM %L CSV NULL ''\N''', path || 'allomorph_components.csv');
 EXECUTE format('COPY value_variants(value_variant_id, value_id, value) FROM %L CSV NULL ''\N''', path || 'value_variants.csv');
 EXECUTE format('COPY values(value_id, sign_id, main_variant_id, phonographic) FROM %L CSV NULL ''\N''', path || 'values.csv');
 
 INSERT INTO signs SELECT DISTINCT sign_id FROM allomorphs;
+EXECUTE format('COPY sign_variants(sign_variant_id, sign_id, allomorph_id, allograph_ids, variant_type, specific) FROM %L CSV NULL ''\N''', path || 'sign_variants.csv');
+INSERT INTO sign_variants_composition SELECT * FROM sign_variants_composition_view;
 
 PERFORM setval('glyphs_glyph_id_seq', max(glyph_id)) FROM glyphs;
 PERFORM setval('graphemes_grapheme_id_seq', max(grapheme_id)) FROM graphemes;
@@ -29,9 +31,8 @@ PERFORM setval('allomorphs_allomorph_id_seq', max(allomorph_id)) FROM allomorphs
 PERFORM setval('values_value_id_seq', max(value_id)) FROM values;
 PERFORM setval('value_variants_value_variant_id_seq', max(value_variant_id)) FROM value_variants;
 PERFORM setval('signs_sign_id_seq', max(sign_id)) FROM signs;
+PERFORM setval('sign_variants_sign_variant_id_seq', max(sign_variant_id)) FROM sign_variants;
 
-INSERT INTO sign_variants (sign_id, allomorph_id, allograph_ids, variant_type, specific) SELECT * FROM sign_variants_view;
-INSERT INTO sign_variants_composition SELECT * FROM sign_variants_composition_view;
 CALL signlist_create_triggers();
 
 END
