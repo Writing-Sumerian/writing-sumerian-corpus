42bfc8257527be993bb220f8bb1b555395d17383
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-11-10 16:00:24 +0100

    Extract template types for corpus tables

--- src/modules/create_corpus/sql/cuneiform_create_corpus.sql
+++ src/modules/create_corpus/sql/cuneiform_create_corpus.sql
@@ -28,6 +28,77 @@ CREATE TYPE block_type AS ENUM (
 );
 
 
+
+CREATE TYPE sections_type AS (
+    transliteration_id integer,
+    section_no integer,
+    section_name text,
+    composition_id integer,
+    witness_type witness_type
+);
+
+CREATE TYPE compounds_type AS (
+    transliteration_id integer,
+    compound_no integer,
+    pn_type pn_type,
+    language LANGUAGE,
+    section_no integer,
+    compound_comment text
+);
+
+CREATE TYPE words_type AS (
+    transliteration_id integer,
+    word_no integer,
+    compound_no integer,
+    capitalized boolean
+);
+
+CREATE TYPE surfaces_type AS (
+    transliteration_id integer,
+    surface_no integer,
+    surface_type surface_type,
+    surface_data text,
+    surface_comment text
+);
+
+CREATE TYPE blocks_type AS (
+    transliteration_id integer,
+    block_no integer,
+    surface_no integer,
+    block_type block_type,
+    block_data text,
+    block_comment text
+);
+
+CREATE TYPE lines_type AS (
+    transliteration_id integer,
+    line_no integer,
+    block_no integer,
+    line text,
+    line_comment text
+);
+
+CREATE TYPE corpus_type AS (
+    transliteration_id integer,
+    sign_no integer,
+    line_no integer,
+    word_no integer,
+    value_id integer,
+    sign_variant_id integer,
+    custom_value text,
+    type sign_type,
+    indicator_type indicator_type,
+    phonographic boolean,
+    stem boolean,
+    condition sign_condition,
+    crits text,
+    comment text,
+    newline boolean,
+    inverted boolean,
+    ligature boolean
+);
+
+
 CREATE OR REPLACE PROCEDURE create_corpus (schema text)
     LANGUAGE PLPGSQL
     AS 
@@ -38,25 +109,24 @@ BEGIN
 EXECUTE format(
     $$
     CREATE TABLE %1$I.sections (
-        transliteration_id integer,
-        section_no integer,
-        section_name text NOT NULL,
-        composition_id integer NOT NULL,
-        witness_type witness_type NOT NULL,
+        LIKE sections_type,
         PRIMARY KEY (transliteration_id, section_no)
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.sections 
+        ALTER COLUMN section_name SET NOT NULL,
+        ALTER COLUMN composition_id SET NOT NULL,
+        ALTER COLUMN witness_type SET NOT NULL
     $$,
     schema);
 
 EXECUTE format(
     $$
     CREATE TABLE %1$I.compounds (
-        transliteration_id integer,
-        compound_no integer,
-        pn_type pn_type,
-        language LANGUAGE,
-        section_no integer,
-        compound_comment text,
+        LIKE compounds_type,
         PRIMARY KEY (transliteration_id, compound_no),
         FOREIGN KEY (transliteration_id, section_no) REFERENCES %1$I.sections (transliteration_id, section_no) DEFERRABLE INITIALLY IMMEDIATE
     )
@@ -66,82 +136,90 @@ EXECUTE format(
 EXECUTE format(
     $$
     CREATE TABLE %1$I.words (
-        transliteration_id integer,
-        word_no integer,
-        compound_no integer NOT NULL,
-        capitalized boolean,
+        LIKE words_type,
         PRIMARY KEY (transliteration_id, word_no),
         FOREIGN KEY (transliteration_id, compound_no) REFERENCES %1$I.compounds (transliteration_id, compound_no) DEFERRABLE INITIALLY IMMEDIATE
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.words 
+        ALTER COLUMN compound_no SET NOT NULL,
+        ALTER COLUMN capitalized SET NOT NULL
     $$,
     schema);
 
 EXECUTE format(
     $$
     CREATE TABLE %1$I.surfaces (
-        transliteration_id integer,
-        surface_no integer,
-        surface_type surface_type NOT NULL,
-        surface_data text,
-        surface_comment text,
+        LIKE surfaces_type,
         PRIMARY KEY (transliteration_id, surface_no)
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.surfaces 
+        ALTER COLUMN surface_type SET NOT NULL
     $$,
     schema);
 
 EXECUTE format(
     $$
     CREATE TABLE %1$I.blocks (
-        transliteration_id integer,
-        block_no integer,
-        surface_no integer NOT NULL,
-        block_type block_type NOT NULL,
-        block_data text,
-        block_comment text,
+        LIKE blocks_type,
         PRIMARY KEY (transliteration_id, block_no),
         FOREIGN KEY (transliteration_id, surface_no) REFERENCES %1$I.surfaces (transliteration_id, surface_no) DEFERRABLE INITIALLY IMMEDIATE
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.blocks 
+        ALTER COLUMN surface_no SET NOT NULL,
+        ALTER COLUMN block_type SET NOT NULL
     $$,
     schema);
 
 EXECUTE format(
     $$
     CREATE TABLE %1$I.lines (
-        transliteration_id integer,
-        line_no integer,
-        block_no integer NOT NULL,
-        line text,
-        line_comment text,
+        LIKE lines_type,
         PRIMARY KEY (transliteration_id, line_no),
         FOREIGN KEY (transliteration_id, block_no) REFERENCES %1$I.blocks (transliteration_id, block_no) DEFERRABLE INITIALLY IMMEDIATE
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.lines 
+        ALTER COLUMN block_no SET NOT NULL
     $$,
     schema);
 
 EXECUTE format(
     $$
     CREATE TABLE %1$I.corpus (
-        transliteration_id integer,
-        sign_no integer,
-        line_no integer NOT NULL,
-        word_no integer NOT NULL,
-        value_id integer REFERENCES values DEFERRABLE INITIALLY IMMEDIATE,
-        sign_variant_id integer REFERENCES sign_variants DEFERRABLE INITIALLY IMMEDIATE,
-        custom_value text,
-        type sign_type NOT NULL,
-        indicator_type indicator_type,
-        phonographic boolean,
-        stem boolean,
-        condition sign_condition NOT NULL,
-        crits text,
-        comment text,
-        newline boolean NOT NULL,
-        inverted boolean NOT NULL,
-        ligature boolean NOT NULL,
+        LIKE corpus_type,
         PRIMARY KEY (transliteration_id, sign_no),
+        FOREIGN KEY (value_id) REFERENCES values DEFERRABLE INITIALLY IMMEDIATE,
+        FOREIGN KEY (sign_variant_id) REFERENCES sign_variants DEFERRABLE INITIALLY IMMEDIATE,
         FOREIGN KEY (transliteration_id, word_no) REFERENCES %1$I.words (transliteration_id, word_no) DEFERRABLE INITIALLY IMMEDIATE,
         FOREIGN KEY (transliteration_id, line_no) REFERENCES %1$I.lines (transliteration_id, line_no) DEFERRABLE INITIALLY IMMEDIATE
     )
+    $$,
+    schema);
+EXECUTE format(
+    $$
+    ALTER TABLE %1$I.corpus 
+        ALTER COLUMN line_no SET NOT NULL,
+        ALTER COLUMN word_no SET NOT NULL,
+        ALTER COLUMN type SET NOT NULL,
+        ALTER COLUMN condition SET NOT NULL,
+        ALTER COLUMN newline SET NOT NULL,
+        ALTER COLUMN inverted SET NOT NULL,
+        ALTER COLUMN ligature SET NOT NULL
     $$,
     schema);
 
