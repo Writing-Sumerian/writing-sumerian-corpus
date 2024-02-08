6203ca1d1180f42b729d22ac3c6d53aedd55f6fb
Marc Endesfelder <marc.endesfelder@gmail.com>
2024-02-08 16:59:06 +0100

    Fix test setup

--- src/test/sql/setup.sql
+++ src/test/sql/setup.sql
@@ -11,6 +11,13 @@ INSERT INTO ensembles VALUES (-1, 'test');
 INSERT INTO texts VALUES (-1, -1, null, null, 'test', null, null, null, null, null, -1, null, null, null, null);
 INSERT INTO transliterations VALUES (-1, -1, -1);
 INSERT INTO compositions (VALUES (-1, 'a'), (-2, 'b'));
+CREATE TABLE errors (
+    transliteration_id integer,
+    line integer,
+    col integer,
+    symbol text,
+    message text
+);
 $BODY$;
 
 
@@ -26,4 +33,5 @@ DELETE FROM ensembles WHERE ensemble_id = -1;
 DELETE FROM genres WHERE genre_id = -1;
 DELETE FROM corpora WHERE corpus_id = -1;
 DELETE FROM compositions WHERE composition_id = -1 OR composition_id = -2;
+DROP TABLE errors;
 $BODY$;
