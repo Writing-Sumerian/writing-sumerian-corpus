e530f0d05ebc71d96f3d95b54a562926f062b291
Marc Endesfelder <marc.endesfelder@gmail.com>
2024-04-29 16:49:02 +0200

    Further improve data entry after parsing

--- src/modules/parser/sql/cuneiform_parser.sql
+++ src/modules/parser/sql/cuneiform_parser.sql
@@ -70,6 +70,25 @@ CREATE TYPE corpus_parser_type AS (
     inverted boolean,
     ligature boolean,
     start_col integer,
+    stop_col integer
+);
+
+CREATE TYPE corpus_parser_ext_type AS (
+    line_no integer, 
+    word_no integer, 
+    value text, 
+    sign_spec text, 
+    type @extschema:cuneiform_sign_properties@.sign_type,  
+    indicator_type @extschema:cuneiform_sign_properties@.indicator_type,
+    phonographic boolean,
+    condition @extschema:cuneiform_sign_properties@.sign_condition,
+    stem boolean,
+    crits text, 
+    comment text,
+    newline boolean,
+    inverted boolean,
+    ligature boolean,
+    start_col integer,
     stop_col integer,
     line_no_code integer, 
     start_col_code integer, 
@@ -100,14 +119,18 @@ corpus_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.corpus 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         line_no, 
         word_no, 
-        NULL, 
-        NULL, 
-        CASE WHEN type = 'value' OR type = 'sign' THEN NULL ELSE value END, 
-        type, 
+        value_id, 
+        sign_variant_id, 
+        CASE WHEN sign_variant_id IS NOT NULL THEN 
+            NULL 
+        ELSE 
+            value  || COALESCE('(' || sign_spec || ')', '') 
+        END, 
+        a.type, 
         indicator_type, 
         phonographic, 
         stem, 
@@ -118,16 +141,17 @@ corpus_plan = plpy.prepare(
         inverted, 
         ligature
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY AS a
+        LEFT JOIN {schema}.corpus_encoder ON transliteration_id = {id} AND sign_no = ordinality-1
     """,
-    ['integer', '@extschema@.corpus_parser_type[]']
+    ['@extschema@.corpus_parser_ext_type[]']
 )
 
 corpus_unencoded_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.corpus_parsed_unencoded
     SELECT
-        $1,
+        {id},
         ordinality::integer-1, 
         value, 
         sign_spec, 
@@ -136,148 +160,136 @@ corpus_unencoded_plan = plpy.prepare(
         start_col_code,
         stop_col_code
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     WHERE
         type = 'value' OR type = 'sign'
         """, 
-    ['integer', '@extschema@.corpus_parser_type[]']
+    ['@extschema@.corpus_parser_ext_type[]']
 )
 
 words_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.words 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         compound_no, 
         capitalized
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     """,
-    ['integer', '@extschema@.words_parser_type[]']
+    ['@extschema@.words_parser_type[]']
 )
 
 compounds_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.compounds 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         pn_type,
         language,
         section_no,
         comment
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     """,
-    ['integer', '@extschema@.compounds_parser_type[]']
+    ['@extschema@.compounds_parser_type[]']
 )
 
 sections_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.sections 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         section_name, 
         composition_id
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
         LEFT JOIN @extschema:cuneiform_corpus@.compositions ON composition_name = composition
     """,
-    ['integer', '@extschema@.sections_parser_type[]']
+    ['@extschema@.sections_parser_type[]']
 )
 
 surfaces_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.surfaces 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         surface, 
         data,
         comment
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     """,
-    ['integer', '@extschema@.surfaces_parser_type[]']
+    ['@extschema@.surfaces_parser_type[]']
 )
 
 blocks_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.blocks 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         surface_no,
         block, 
         data,
         comment
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     """,
-    ['integer', '@extschema@.blocks_parser_type[]']
+    ['@extschema@.blocks_parser_type[]']
 )
 
 lines_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.lines 
     SELECT 
-        $1, 
+        {id}, 
         ordinality::integer-1, 
         block_no, 
         line,
         comment
     FROM
-        UNNEST($2) WITH ORDINALITY
+        UNNEST($1) WITH ORDINALITY
     """,
-    ['integer', '@extschema@.lines_parser_type[]']
+    ['@extschema@.lines_parser_type[]']
 )
 
 errors_plan = plpy.prepare(
     f"""
     INSERT INTO {schema}.errors 
     SELECT 
-        $1, 
+        {id}, 
         line_no, 
         "column", 
         symbol, 
         msg
     FROM
-        UNNEST($2)
+        UNNEST($1)
     """,
-    ['integer', '@extschema@.errors_parser_type[]']
+    ['@extschema@.errors_parser_type[]']
 )
 
 surfaces, blocks, lines, signs, compounds, words, sections, errors = parseText(code)
 
 plpy.execute(f"DELETE FROM {schema}.corpus_parsed_unencoded WHERE transliteration_id = {id}")
 
-plpy.execute(sections_plan, [id, list(sections.to_records(index=False))])
-plpy.execute(compounds_plan, [id, [(x[0], x[1], x[2] if x[2] is not pd.NA else None, x[3]) for x in compounds.itertuples(index=False)]])
-plpy.execute(words_plan, [id, list(words.to_records(index=False))])
+plpy.execute(sections_plan, [list(sections.to_records(index=False))])
+plpy.execute(compounds_plan, [[(x[0], x[1], x[2] if x[2] is not pd.NA else None, x[3]) for x in compounds.itertuples(index=False)]])
+plpy.execute(words_plan, [list(words.to_records(index=False))])
 
-plpy.execute(surfaces_plan, [id, list(surfaces.to_records(index=False))])
-plpy.execute(blocks_plan, [id, list(blocks.to_records(index=False))])
-plpy.execute(lines_plan, [id, list(lines.to_records(index=False))])
+plpy.execute(surfaces_plan, [list(surfaces.to_records(index=False))])
+plpy.execute(blocks_plan, [list(blocks.to_records(index=False))])
+plpy.execute(lines_plan, [list(lines.to_records(index=False))])
 
 signs_tup = list(signs.to_records(index=False))
-plpy.execute(corpus_plan, [id, signs_tup])
-plpy.execute(corpus_unencoded_plan, [id, signs_tup])
+plpy.execute(corpus_unencoded_plan, [signs_tup])
+plpy.execute(corpus_plan, [signs_tup])
 
-plpy.execute(errors_plan, [id, list(errors.to_records(index=False))])
-
-plpy.execute(f"""
-    UPDATE {schema}.corpus SET 
-        value_id = a.value_id, 
-        sign_variant_id = a.sign_variant_id 
-    FROM 
-        {schema}.corpus_encoder a 
-    WHERE 
-        corpus.transliteration_id = a.transliteration_id AND 
-        corpus.transliteration_id = {id} AND
-        corpus.sign_no = a.sign_no
-    """)
+plpy.execute(errors_plan, [list(errors.to_records(index=False))])
 
 plpy.execute(f"""
     DELETE FROM {schema}.corpus_parsed_unencoded
@@ -287,17 +299,6 @@ plpy.execute(f"""
         corpus.transliteration_id = {id} AND
         corpus.sign_no = corpus_parsed_unencoded.sign_no AND
         sign_variant_id IS NOT NULL
-    """)
-
-plpy.execute(f"""
-    UPDATE {schema}.corpus SET 
-        custom_value = corpus_parsed_unencoded.value  || COALESCE('(' || corpus_parsed_unencoded.sign_spec || ')', '')
-    FROM
-        {schema}.corpus_parsed_unencoded
-    WHERE
-        corpus.transliteration_id = corpus_parsed_unencoded.transliteration_id AND
-        corpus.transliteration_id = {id} AND
-        corpus.sign_no = corpus_parsed_unencoded.sign_no;
     """)
 
 $BODY$;
