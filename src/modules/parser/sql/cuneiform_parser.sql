d3e6de92b50345a4b3123e341af2e658ae12dc82
Marc Endesfelder <marc.endesfelder@gmail.com>
2024-04-29 11:00:01 +0200

    Improve data entry after parsing

--- src/modules/parser/sql/cuneiform_parser.sql
+++ src/modules/parser/sql/cuneiform_parser.sql
@@ -11,6 +11,73 @@ CREATE TABLE corpus_parsed_unencoded (
 CALL @extschema:cuneiform_encoder@.create_corpus_encoder('parser_corpus_encoder', 'corpus_parsed_unencoded', '{transliteration_id}', '@extschema@');
 
 
+CREATE TYPE sections_parser_type AS (
+    section_name text,
+    composition text
+);
+
+CREATE TYPE compounds_parser_type AS (
+    pn_type @extschema:cuneiform_sign_properties@.pn_type,
+    language @extschema:cuneiform_sign_properties@.language,
+    section_no integer,
+    comment text
+);
+
+CREATE TYPE words_parser_type AS (
+    compound_no integer,
+    capitalized boolean
+);
+
+CREATE TYPE surfaces_parser_type AS (
+    surface @extschema:cuneiform_create_corpus@.surface_type, 
+    data text,
+    comment text
+);
+
+CREATE TYPE blocks_parser_type AS (
+    surface_no integer, 
+    block @extschema:cuneiform_create_corpus@.block_type, 
+    data text, 
+    comment text
+);
+
+CREATE TYPE lines_parser_type AS (
+    block_no integer, 
+    line text, 
+    comment text
+);
+
+CREATE TYPE corpus_parser_type AS (
+    line_no integer, 
+    word_no integer, 
+    value text, 
+    sign_spec text, 
+    type @extschema:cuneiform_sign_properties@.sign_type,  
+    indicator_type @extschema:cuneiform_sign_properties@.indicator_type,
+    phonographic boolean,
+    condition @extschema:cuneiform_sign_properties@.sign_condition,
+    stem boolean,
+    crits text, 
+    comment text,
+    newline boolean,
+    inverted boolean,
+    ligature boolean,
+    start_col integer,
+    stop_col integer,
+    line_no_code integer, 
+    start_col_code integer, 
+    stop_col_code integer
+);
+
+CREATE TYPE errors_parser_type AS (
+    line_no integer, 
+    "column" integer, 
+    symbol text, 
+    msg text
+);
+
+
+
 CREATE OR REPLACE PROCEDURE parse (
         code text, 
         schema text,
@@ -23,76 +90,175 @@ from cuneiformparser import parseText
 import pandas as pd
 
 corpus_plan = plpy.prepare(
-    f"INSERT INTO {schema}.corpus VALUES ($1, $2, $3, $4, NULL, NULL, CASE WHEN $6 = 'value' OR $6 = 'sign' THEN NULL ELSE $5 END, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)", 
-    ['integer', 'integer', 'integer', 'integer', 'text', '@extschema:cuneiform_sign_properties@.sign_type', '@extschema:cuneiform_sign_properties@.indicator_type', 'boolean', 'boolean', '@extschema:cuneiform_sign_properties@.sign_condition', 'text', 'text', 'boolean', 'boolean', 'boolean']
+    f"""
+    INSERT INTO {schema}.corpus 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        line_no, 
+        word_no, 
+        NULL, 
+        NULL, 
+        CASE WHEN type = 'value' OR type = 'sign' THEN NULL ELSE value END, 
+        type, 
+        indicator_type, 
+        phonographic, 
+        stem, 
+        condition, 
+        crits, 
+        comment, 
+        newline, 
+        inverted, 
+        ligature
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.corpus_parser_type[]']
 )
 
 corpus_unencoded_plan = plpy.prepare(
-    f'INSERT INTO @extschema@.corpus_parsed_unencoded VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
-    ['integer', 'integer', 'text', 'text', '@extschema:cuneiform_sign_properties@.sign_type', 'integer', 'integer', 'integer']
+    f"""
+    INSERT INTO @extschema@.corpus_parsed_unencoded
+    SELECT
+        $1,
+        ordinality::integer-1, 
+        value, 
+        sign_spec, 
+        type,
+        line_no_code,
+        start_col_code,
+        stop_col_code
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    WHERE
+        type = 'value' OR type = 'sign'
+        """, 
+    ['integer', '@extschema@.corpus_parser_type[]']
 )
 
 words_plan = plpy.prepare(
-    f'INSERT INTO {schema}.words VALUES ($1, $2, $3, $4)',
-    ['integer', 'integer', 'integer', 'boolean']
+    f"""
+    INSERT INTO {schema}.words 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        compound_no, 
+        capitalized
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.words_parser_type[]']
 )
 
 compounds_plan = plpy.prepare(
-    f'INSERT INTO {schema}.compounds VALUES ($1, $2, $3, $4, $5, $6)',
-    ['integer', 'integer', '@extschema:cuneiform_sign_properties@.pn_type', '@extschema:cuneiform_sign_properties@.language', 'integer', 'text']
+    f"""
+    INSERT INTO {schema}.compounds 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        pn_type,
+        language,
+        section_no,
+        comment
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.compounds_parser_type[]']
 )
 
 sections_plan = plpy.prepare(
-    f'INSERT INTO {schema}.sections SELECT $1, $2, $3, composition_id FROM @extschema:cuneiform_corpus@.compositions WHERE composition_name = $4',
-    ['integer', 'integer', 'text', 'text']
+    f"""
+    INSERT INTO {schema}.sections 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        section_name, 
+        composition_id
+    FROM
+        UNNEST($2) WITH ORDINALITY
+        LEFT JOIN @extschema:cuneiform_corpus@.compositions ON composition_name = composition
+    """,
+    ['integer', '@extschema@.sections_parser_type[]']
 )
 
 surfaces_plan = plpy.prepare(
-    f'INSERT INTO {schema}.surfaces (transliteration_id, surface_no, surface_type, surface_data, surface_comment) VALUES ($1, $2, $3, $4, $5)',
-    ['integer', 'integer', '@extschema:cuneiform_create_corpus@.surface_type', 'text', 'text']
+    f"""
+    INSERT INTO {schema}.surfaces 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        surface, 
+        data,
+        comment
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.surfaces_parser_type[]']
 )
 
 blocks_plan = plpy.prepare(
-    f'INSERT INTO {schema}.blocks (transliteration_id, block_no, surface_no, block_type, block_data, block_comment) VALUES ($1, $2, $3, $4, $5, $6)',
-    ['integer', 'integer', 'integer', '@extschema:cuneiform_create_corpus@.block_type', 'text', 'text']
+    f"""
+    INSERT INTO {schema}.blocks 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        surface_no,
+        block, 
+        data,
+        comment
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.blocks_parser_type[]']
 )
 
 lines_plan = plpy.prepare(
-    f'INSERT INTO {schema}.lines VALUES ($1, $2, $3, $4, $5)',
-    ['integer', 'integer', 'integer', 'text', 'text']
+    f"""
+    INSERT INTO {schema}.lines 
+    SELECT 
+        $1, 
+        ordinality::integer-1, 
+        block_no, 
+        line,
+        comment
+    FROM
+        UNNEST($2) WITH ORDINALITY
+    """,
+    ['integer', '@extschema@.lines_parser_type[]']
 )
 
 errors_plan = plpy.prepare(
-    f'INSERT INTO {schema}.errors VALUES ($1, $2, $3, $4, $5)',
-    ['integer', 'integer', 'integer', 'text', 'text']
+    f"""
+    INSERT INTO {schema}.errors 
+    SELECT 
+        $1, 
+        line_no, 
+        "column", 
+        symbol, 
+        msg
+    FROM
+        UNNEST($2)
+    """,
+    ['integer', '@extschema@.errors_parser_type[]']
 )
 
 surfaces, blocks, lines, signs, compounds, words, sections, errors = parseText(code)
 
 plpy.execute(f"DELETE FROM @extschema@.corpus_parsed_unencoded WHERE transliteration_id = {id}")
 
-for ix, row in sections.iterrows():
-    plpy.execute(sections_plan, [id, ix]+[row[key] for key in ['section_name', 'composition']])
-for ix, row in compounds.iterrows():
-    plpy.execute(compounds_plan, [id, ix]+[row[key] if row[key] is not pd.NA else None for key in ['pn_type', 'language', 'section_no', 'comment']])
-for ix, row in words.iterrows():
-    plpy.execute(words_plan, [id, ix]+[row[key] for key in ['compound_no', 'capitalized']])
+plpy.execute(sections_plan, [id, list(sections.to_records(index=False))])
+plpy.execute(compounds_plan, [id, [(x[0], x[1], x[2] if x[2] is not pd.NA else None, x[3]) for x in compounds.itertuples(index=False)]])
+plpy.execute(words_plan, [id, list(words.to_records(index=False))])
 
-for ix, row in surfaces.iterrows():
-    plpy.execute(surfaces_plan, [id, ix]+[row[key] for key in ['surface', 'data', 'comment']])
-for ix, row in blocks.iterrows():
-    plpy.execute(blocks_plan, [id, ix]+[row[key] for key in ['surface_no', 'block', 'data', 'comment']])
-for ix, row in lines.iterrows():
-    plpy.execute(lines_plan, [id, ix]+[row[key] for key in ['block_no', 'line', 'comment']])
+plpy.execute(surfaces_plan, [id, list(surfaces.to_records(index=False))])
+plpy.execute(blocks_plan, [id, list(blocks.to_records(index=False))])
+plpy.execute(lines_plan, [id, list(lines.to_records(index=False))])
 
-for ix, row in signs.iterrows():
-    plpy.execute(corpus_plan, [id, ix]+[row[key] for key in ['line_no', 'word_no', 'value', 'type', 'indicator_type', 'phonographic', 'stem', 'condition', 'crits', 'comment', 'newline', 'inverted', 'ligature']])
-for ix, row in signs.iterrows():
-    if row['type'] in ['value', 'sign']:
-        plpy.execute(corpus_unencoded_plan, [id, ix]+[row[key] for key in ['value', 'sign_spec', 'type', 'line_no_code', 'start_col_code', 'stop_col_code']])
+signs_tup = list(signs.to_records(index=False))
+plpy.execute(corpus_plan, [id, signs_tup])
+plpy.execute(corpus_unencoded_plan, [id, signs_tup])
 
-for ix, row in errors.iterrows():
-    plpy.execute(errors_plan, [id]+[row[key] for key in ['line_no', 'column', 'symbol', 'msg']])
+plpy.execute(errors_plan, [id, list(errors.to_records(index=False))])
 
 plpy.execute(f"""
     UPDATE {schema}.corpus SET 
