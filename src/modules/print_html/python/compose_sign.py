6102ae7ed75aec235be3a2db24637f4463a4a934
Marc Endesfelder <marc.endesfelder@gmail.com>
2024-07-06 10:09:00 +0200

    Add 'd' modifier to compose_sign_html

--- src/modules/print_html/python/compose_sign.py
+++ src/modules/print_html/python/compose_sign.py
@@ -8,7 +8,7 @@ def compose_sign_html (tree:jsonb) -> str:
     import re
 
     precedence = {'.': 0, '+': 1, '×': 2, '&': 3, '%': 4, '@': 4}
-    modifiers = {'g': 'gunû', 'š': 'šeššig', 't': 'tenû', 'n': 'nutillû', 'k': 'kabatenû', 'z': 'zidatenû', 'i': 'inversum', 'v': 'var', 'c': '○︎', '4': 'quad'}
+    modifiers = {'g': 'gunû', 'š': 'šeššig', 't': 'tenû', 'n': 'nutillû', 'k': 'kabatenû', 'z': 'zidatenû', 'i': 'inversum', 'v': 'var', 'c': '○︎', '4': 'quad', 'd': 'doubled'}
 
     def stack(a, b):
         return f'<span class="stack">{a}<br/>{b}</span>'
